6bdb0aed3c9efc25e9188def71cfe79e479d9f0c
callum.cooper <callum.cooper@digital.hmrc.gov.uk>
2016-04-21 15:14:55 +0100

    initial commit

---
+++ engine/main.cpp
@@ -0,0 +1,14 @@
+// main.cpp acts as an entry point to the application.
+
+// Include headers
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "DirectX-engine" << endl;
+
+	cin.ignore();
+	return 0;
+}
